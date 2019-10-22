03ee07993ca5dd66c4ac7b858380f7d84eddbfcd
Valeriy Grechukha <v.mrfriz@gmail.com>
2019-10-22 14:13:07 +0300

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,25 @@
 |
 */
 
+/**
+ * SubscribeController
+ */
 Route::resource('/subscribe', 'SubscriberController');
 
+/**
+ * OrderController
+ */
 Route::get('/', 'OrderController@index');
 Route::post('/', 'OrderController@store');//->middleware('auth');
 Route::get('/create', 'OrderController@create');//->middleware('auth');
+// Show order
+Route::get('/id{id}', 'OrderController@show');
 Route::get('/{db}_{id}', 'OrderController@show');
+// Reserve order
+Route::post('/id{id}', 'OrderController@reserve');
+Route::post('/{db}_{id}', 'OrderController@reserve');
+Route::get('/reserved', 'OrderController@reserved');
+//
 Route::put('/{db}_{id}', 'OrderController@update');//->middleware('auth');
 Route::delete('/{db}_{id}', 'OrderController@destroy');//->middleware('auth');
 Route::get('/edit/{db}_{id}', 'OrderController@edit');//->middleware('auth');
